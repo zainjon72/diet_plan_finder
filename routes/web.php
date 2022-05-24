7fe32959de15f54a988311de8ef2ab54b20e8c7b
Hassan <hassanmujtaba4632@gmail.com>
2022-05-24 18:10:48 +0500

    push

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->middleware('admin_area')->group(function(){
+	Route::get('/home', 'HomeController@index')->name('home');
+});
