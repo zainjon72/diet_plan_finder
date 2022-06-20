1878e0b9e6ff17168c2f80ebe2d4205b484d3f72
Hassan <hassanmujtaba4632@gmail.com>
2022-06-20 16:04:11 +0500

    push

--- routes/web.php
+++ routes/web.php
@@ -21,62 +21,102 @@ Auth::routes();
 
 Route::prefix('admin')->middleware('admin_area')->group(function(){
 	Route::get('/home', 'UserController@index');
+
 	Route::get('/plan', 'UserController@plan');
+
 	Route::get('/health', 'HealthConditionController@index');
+
 	Route::post('/health', 'HealthConditionController@create');
+
 	Route::get('/add_health', 'HealthConditionController@add_health');
+
 	Route::get('/delete_health/{id}', 'HealthConditionController@delete');
+
 	Route::get('/edit_health/{id}', 'HealthConditionController@edit');
+
 	Route::post('/edit_health/{id}', 'HealthConditionController@edit');
+
 	Route::get('/delete_user/{id}', 'UserController@delete');
 	// Route::get('/edit_user_admin/{id}', 'UserController@admin_role');
 	// Route::get('/edit_user_nutritionist/{id}', 'UserController@nutritionist_role');
 	Route::get('/approve/{id}', 'UserController@approve');
+
 	Route::get('/approve_plan/{id}', 'UserController@approve_plan');
+
 	Route::get('/delete_plan/{id}', 'UserController@delete_plan');
+
 	Route::get('/cancel_plan/{id}', 'UserController@cancel_plan');
+
 	Route::get('/meal/{id}', 'Nutritionist\\DietPlanController@viewmeal');
+
 	Route::get('/approve_meal/{id}', 'Nutritionist\\MealController@approve');
+
 	Route::get('/cancel_meal/{id}', 'Nutritionist\\MealController@cancel_meal');
 });
 Route::prefix('nutritionist')->middleware('nutritionist')->group(function(){
 	Route::get('/home', 'Nutritionist\\DietPlanController@index');
+
 	Route::post('/home', 'Nutritionist\\DietPlanController@create');
+
 	Route::get('/add_plan', 'Nutritionist\\DietPlanController@add_plan');
+
 	Route::get('/delete_plan/{id}', 'Nutritionist\\DietPlanController@delete');
+
 	Route::get('/approve/{id}', 'Nutritionist\\DietPlanController@approve');
+
 	Route::get('/edit_plan/{id}', 'Nutritionist\\DietPlanController@edit');
+
 	Route::post('/edit_plan/{id}', 'Nutritionist\\DietPlanController@edit');
 	// Route::post('/add_plan', 'Nutritionist\\DietPlanController@create');
 	Route::get('/meal', 'Nutritionist\\MealController@index');
+
 	Route::post('/meal', 'Nutritionist\\MealController@create');
+
 	Route::get('/add_meal', 'Nutritionist\\MealController@add_meal');
+
 	Route::get('/delete_meal/{id}', 'Nutritionist\\MealController@delete');
+
 	Route::get('/update_meal/{id}', 'Nutritionist\\MealController@update');
+
 	Route::post('/update_meal/{id}', 'Nutritionist\\MealController@update');
 });
 
 Route::middleware(['auth', 'customer'])->group(function(){
 	Route::get('/cart', 'CartItemController@index');
+
 	Route::post('/cart', 'CartItemController@create');
+
 	Route::get('/checkout', 'CartItemController@checkout');
+
 	Route::post('/checkout', 'CartItemController@checkout');
+
 	Route::get('/delete_cart', 'CartItemController@delete_all');
+
 	Route::get('/profile', 'UserController@profile');
+
 	Route::get('stripe', 'StripePaymentController@stripe');
+
 	Route::post('stripe', 'StripePaymentController@stripePost')->name('stripe.post');
+
 	Route::get('/my-plan', 'MyPlansController@index');
 	// Route::post('/my-plan', 'MyPlansController@my_plan');
 	Route::get('/meal/{id}', 'MyPlansController@my_meal');
+
 	Route::get('/view_plan/{id}', 'MyPlansController@view_plans');
+
 	Route::get('/thank_you', 'HomeController@thankyou');
+
 	Route::post('/plan/{id}', 'FeedbackController@create');
+
 	Route::get('/plan/{id}', 'HomeController@plan');
 });
 
 Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/plan', 'HomeController@plans');
+
 Route::get('/delete_plan/{id}', 'CartItemController@delete');
 // Route::get('/', 'DietPlanController@show');
 
